1bcf610cc2ee46ae9c78a2fd3651ac8bd001d766
kuwaharakengo <kengo.kuwahara@rakus.co.jp>
2017-06-14 16:14:20 +0900

    ページ遷移用のコントローラ作成

--- src/main/java/com/example/demo/PageController.java
+++ src/main/java/com/example/demo/PageController.java
@@ -1,5 +1,49 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ページ遷移のコントローラ.
+ * @author Kengo Kuwahara
+ *
+ */
+@RequestMapping("/")
 public class PageController {
 
+	/**
+	 * トップページを表示.
+	 * @return
+	 */
+	@RequestMapping("/")
+	public String topPage() {
+		return "topPage";
+	}
+	
+	/**
+	 * 八島の自己紹介ページへ遷移.
+	 * @return
+	 */
+	@RequestMapping("/yashima")
+	public String yashima(){
+		return "yashima";
+	}
+
+	/**
+	 * 桑原の自己紹介ページへ遷移.
+	 * @return
+	 */
+	@RequestMapping("/kuwahara")
+	public String kuwahara(){
+		return "kuwahara";
+	}
+	
+	/**
+	 * 渕本の自己紹介ページへ遷移.
+	 * @return
+	 */
+	@RequestMapping("/hutimoto")
+	public String hutimoto(){
+		return "hutimoto";
+	}
+	
 }
